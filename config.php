d102858ce75caac64611430dac999cc5a5cce1a1
William Floyd <william.png2000@gmail.com>
2017-07-29 20:25:57 +1000

    various: Lots of work, now implements buttons for my various pages.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- config.php
+++
@@ -1,3 +0,0 @@
-$server_name = "Serverinator";
-$server_desc = "Serves things";
-
---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+    $server_name = "Serverinator";
+    $server_desc = "Serves things";
+    $color_bg = "#222";
+    $color_name = "#fff";
+    $color_text = "#ccc";
+?>
+
+<html>
+
+    <head>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.fittext.js"></script>
+        <script src="js/scripts.js"></script>
+        <title><?php echo $server_name; ?></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+    </head>
+
+    <body>
