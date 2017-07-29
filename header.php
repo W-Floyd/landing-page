07a43ffd8868432fa9b7640fcd209861fe75d661
William Floyd <william.png2000@gmail.com>
2017-07-29 20:38:15 +1000

    images: Hopefully makes image titles styled independantly.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- header.php
+++ header.php
@@ -13,9 +13,9 @@
         <script src="js/bootstrap.min.js"></script>
         <script src="js/jquery.fittext.js"></script>
         <script src="js/scripts.js"></script>
-        <title><?php echo $server_name; ?></title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/custom.css" rel="stylesheet">
+        <title><?php echo $server_name; ?></title>
     </head>
 
     <body>
--- index.php
+++ index.php
@@ -5,9 +5,15 @@
     $db = new SQLite3('landingpage.db');
 
     $results = $db->query('SELECT extension, description, image, fullname FROM sites ORDER BY fullname');
+
+
 ?>
 
-<center>
+<section>
+	<h1><?php echo $server_name; ?></h1>
+	<p><?php echo $server_desc; ?></p>
+</section>
+
 <div class="row">
 
 <?php
@@ -19,8 +25,8 @@
         $fullname = $row[3];
         echo '<div class="image-tile col-lg-4 col-sm-5 col-xs-6">';
         echo '<a href="../' . $extension . '"><img class="realimage" src="img/' . $image . '"></a>';
-        echo '<h1 id="title-' . $extension . '">' . $fullname . '</h1><br>';
-        echo '<h2 id="description-' . $extension . '">' . $description . '</h2>';
+        echo '<h1 class="image-title" id="title-' . $extension . '">' . $fullname . '</h1><br>';
+        echo '<h2 class="image-description" id="description-' . $extension . '">' . $description . '</h2>';
         echo '</div>';
         echo '<script type="text/javascript">';
             echo '$("#title-' . $extension . '").fitText();';
@@ -31,7 +37,6 @@
 ?>
 
 </div>
-</center>
 
 <?php
     include_once('footer.php');
