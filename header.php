75b63d2eb837a180f74c7aa03d2182e087721d75
William Floyd <william.png2000@gmail.com>
2017-07-29 23:04:31 +1000

    Cleanup.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- header.php
+++ header.php
@@ -12,7 +12,6 @@
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/jquery.fittext.js"></script>
-        <script src="js/scripts.js"></script>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/custom.css" rel="stylesheet">
         <title><?php echo $server_name; ?></title>
