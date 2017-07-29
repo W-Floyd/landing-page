4e16d94bd83f935071111e4ba7bb524732694533
William Floyd <william.png2000@gmail.com>
2017-07-30 00:32:39 +1000

    header: Updates description and name.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
-    $server_name = "Serverinator";
-    $server_desc = "Serves things";
+    $server_name = "Server-inator";
+    $server_desc = "The most diabolical of machines, dedicated to destroying semiaquatic mammals";
     $color_bg = "#222";
     $color_name = "#fff";
     $color_text = "#ccc";
