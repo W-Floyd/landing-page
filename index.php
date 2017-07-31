1f12cadd26be5f2093bdcd04a55bfbf9059abf87
William Floyd <william.png2000@gmail.com>
2017-07-31 17:12:46 +1000

    index: Fittext correctly.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- index.php
+++ index.php
@@ -10,11 +10,12 @@
 ?>
 
 <section>
-	<h1 class="textcenter main-title"><?php echo $server_name; ?></h1>
-	<h2 class="textcenter main-description"><?php echo $server_desc; ?>
+	<h1 id='main-title' class="textcenter main-title"><?php echo $server_name; ?></h1>
+	<h2 id='main-description' class="textcenter main-description"><?php echo $server_desc; ?>
 
     <script type="text/javascript">'
-        $("#textcenter").fitText();
+        $("#main-title").fitText();
+        $("#main-description").fitText();
     </script>
 </section>
 
