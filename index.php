4cfd34fd4f174f395c2626dcc0a8c424f316fd26
William Floyd <william.png2000@gmail.com>
2017-08-01 23:43:43 +1000

    index: Format newlines as html breaks, so databse works.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 
 
 <h1 class="textcenter main-title">
-    <?php echo $server_name; ?>
+    <?php echo nl2br($server_name); ?>
 </h1>
 <h2 class="textcenter main-description">
-    <?php echo $server_desc; ?>
+    <?php echo nl2br($server_desc); ?>
 </h2>
 
 
@@ -39,8 +39,8 @@
             $fullname = $row[3];
             echo '<div class="image-tile col-lg-3 col-xs-6">';
             echo '<a href="../' . $extension . '"><img class="realimage" src="' . $image_use . '"></a>';
-            echo '<h1 class="image-title textcenter" id="title-' . $checksum . '">' . $fullname . '</h1><br>';
-            echo '<h2 class="image-description textcenter" id="description-' . $checksum . '">' . $description . '</h2>';
+            echo '<h1 class="image-title textcenter" id="title-' . $checksum . '">' . nl2br($fullname) . '</h1><br>';
+            echo '<h2 class="image-description textcenter" id="description-' . $checksum . '">' . nl2br($description) . '</h2>';
             echo '</div>';
             echo '<script type="text/javascript">';
                 echo '$("#title-' . $checksum . '").fitText();';
