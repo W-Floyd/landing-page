c9e0df8f1c31f87bd8a8ffda1c8c7fd325a96b47
William Floyd <william.png2000@gmail.com>
2017-08-25 09:36:49 +1000

    img: Remove all backgrounds and implement as css.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
     $color_bg = "#222";
     $color_name = "#fff";
     $color_text = "#ccc";
+    $image_grad_start = "#4d4d4d";
+    $image_grad_end = "#434343";
 ?>
 
 <html>
@@ -30,6 +32,28 @@
 
         h2 {
             color: <?php echo $color_text; ?>;
+        }
+        
+        .image-tile {
+            /* fallback */
+            background-color: <?php echo $image_grad_start; ?>;
+            
+
+            /* Safari 4-5, Chrome 1-9 */
+            background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(<?php echo $image_grad_start; ?>), to(<?php echo $image_grad_end; ?>));
+
+            /* Safari 5.1, Chrome 10+ */
+            background: -webkit-linear-gradient(top, <?php echo $image_grad_end; ?>, <?php echo $image_grad_start; ?>);
+
+            /* Firefox 3.6+ */
+            background: -moz-linear-gradient(top, <?php echo $image_grad_end; ?>, <?php echo $image_grad_start; ?>);
+
+            /* IE 10 */
+            background: -ms-linear-gradient(top, <?php echo $image_grad_end; ?>, <?php echo $image_grad_start; ?>);
+
+            /* Opera 11.10+ */
+            background: -o-linear-gradient(top, <?php echo $image_grad_end; ?>, <?php echo $image_grad_start; ?>);
+            
         }
 
     </style>
