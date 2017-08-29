ad84636e51c1e3f27b0bf6c9aeab1813cff2768a
William Floyd <william.png2000@gmail.com>
2017-08-29 23:09:53 +1000

    config: Minor change, mostly to re-test git hooks.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- template_config.php
+++ template_config.php
@@ -11,7 +11,7 @@
         hidden
 
     */
-    $server_name = "The Server-inator";
+    $server_name = "The Server-inator!";
     $server_desc = "The most diabolical of machines, dedicated to destroying semiaquatic mammals.";
     $color_bg = "#232E33";
     $color_title = "#fff";
