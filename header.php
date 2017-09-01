ad4f363e0c98896c60e263ce5f2168f6f4c0e0be
William Floyd <william.png2000@gmail.com>
2017-09-01 13:18:17 +1000

    css: Remove bootstrap js and use grid only CSS.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
 
     <head>
         <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
         <script src="js/jquery.fittext.js"></script>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/custom.css" rel="stylesheet">
