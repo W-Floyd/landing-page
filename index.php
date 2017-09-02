940d1b265c563e614c2e9ed71b93cc164bed927d
William Floyd <william.png2000@gmail.com>
2017-09-02 12:47:46 +1000

    index: Try what SO said...
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- index.php
+++ index.php
@@ -45,11 +45,13 @@
                 }
 
                 echo '<div class="image-tile col-lg-2 col-md-3 col-sm-4 col-xs-6">';
+                    echo '<a style="display:block" href="../' . $extension . '">'
                     echo '<div class="tile-bg">';
-                        echo '<a href="../' . $extension . '"><img class="realimage" src="' . $image_use . '"></a>';
+                        echo '<img class="realimage" src="' . $image_use . '">';
                         echo '<h1 class="image-title textcenter" id="title-' . $id . '">' . nl2br($fullname) . '</h1><br>';
                         echo '<h2 class="image-description textcenter" id="description-' . $id . '">' . nl2br($description) . '</h2>';
                     echo '</div>';
+                    echo '</a>';
                 echo '</div>';
                 echo '<script type="text/javascript">';
                     echo 'fitText(document.getElementById("title-' . $id . '"), 1.0);';
